fcdf578fcfbb25c7a672017bcb22870ff4e42f55
Matheus <mhas1311@gmail.com>
2025-07-29 21:21:14 -0300

    Updated corsConfigurer

--- Liga-de-Futebol/src/main/java/com/cefet/Liga/de/Futebol/config/SecurityConfig.java
+++ Liga-de-Futebol/src/main/java/com/cefet/Liga/de/Futebol/config/SecurityConfig.java
@@ -120,7 +120,7 @@ public class SecurityConfig {
 	      @Override
 	      public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	          .allowedOrigins("http://localhost:4200")
+	          .allowedOrigins("https://ligafutebol.netlify.app")
 	          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	          .allowedHeaders("*")
 	          .allowCredentials(true);
